707f5f3c232752bb11cc3cd5d1299fae3006573a
QCY <416665079@qq.com>
2016-01-23 16:28:05 +0800

    建立分支canvas

--- sokaban_node/scripts/common/counts.js
+++
@@ -1,9 +0,0 @@
-var counts = {
-    nodeMapPath: '.././maps/',
-    webMapPath: './maps/',
-    fileLists: [
-        'map1.txt', 'map2.txt', 'map3.txt', 'map4.txt', 'map5.txt'
-    ]
-};
-
-module.exports = counts;
