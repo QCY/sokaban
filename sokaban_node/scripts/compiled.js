aeb976cfbe0afca4902345795d7f2dbcf3bf68e3
QCY <416665079@qq.com>
2016-01-23 16:20:36 +0800

    web版移植进node

--- sokaban_node/scripts/compiled.js
+++ sokaban_node/scripts/compiled.js
@@ -2825,6 +2825,7 @@ var gameManager = (function() {
     };
 
     var addListener = function() {
+        //绑定事件
         player.listen('onMove', player.moveControl.bind(player));
         mapManager.listen('onMatrixChange', mapManager.render.bind(
             mapManager));
@@ -2833,6 +2834,7 @@ var gameManager = (function() {
     };
 
     var removeListener = function() {
+        //解除事件
         player.remove('onMove', player.moveControl);
         mapManager.remove('onMatrixChange', mapManager.render);
         target.remove('boxOnTarget', target.addBox);
@@ -3012,6 +3014,7 @@ var ControlManager = require('./controlManager.js');
 
 var keydownControlerForWeb = function() {
     ControlManager.keydownControler.apply(this);
+    this.configKey();
 };
 
 keydownControlerForWeb.prototype = new ControlManager.keydownControler();
@@ -3027,6 +3030,31 @@ keydownControlerForWeb.prototype.control = function() {
         }
 
     }.bind(this);
+};
+
+keydownControlerForWeb.prototype.configKey = function() {
+    var btn = document.querySelector('[name="button"]');
+
+    btn.onclick = function() {
+        keydownCommands = {};
+        var upValue = Counts.keyCode[document.querySelector(
+                '[name="up"]').value.toUpperCase()],
+            downValue = Counts.keyCode[document.querySelector(
+                '[name="down"]').value.toUpperCase()],
+            rightValue = Counts.keyCode[document.querySelector(
+                '[name="right"]').value.toUpperCase()],
+            leftValue = Counts.keyCode[document.querySelector(
+                '[name="left"]').value.toUpperCase()];
+
+        if (upValue && downValue && rightValue && leftValue) {
+            keydownCommands[upValue] = 'moveUp';
+            keydownCommands[rightValue] = 'moveRight';
+            keydownCommands[downValue] = 'moveDown';
+            keydownCommands[leftValue] = 'moveLeft';
+        } else {
+            alert('按键配置有误，请重新配置');
+        }
+    };
 };
 
 module.exports = {
--- sokaban_node/scripts/partials/controlManagerForWeb.js
+++ sokaban_node/scripts/partials/controlManagerForWeb.js
@@ -2,6 +2,7 @@ var ControlManager = require('./controlManager.js');
 
 var keydownControlerForWeb = function() {
     ControlManager.keydownControler.apply(this);
+    this.configKey();
 };
 
 keydownControlerForWeb.prototype = new ControlManager.keydownControler();
@@ -17,6 +18,31 @@ keydownControlerForWeb.prototype.control = function() {
         }
 
     }.bind(this);
+};
+
+keydownControlerForWeb.prototype.configKey = function() {
+    var btn = document.querySelector('[name="button"]');
+
+    btn.onclick = function() {
+        keydownCommands = {};
+        var upValue = Counts.keyCode[document.querySelector(
+                '[name="up"]').value.toUpperCase()],
+            downValue = Counts.keyCode[document.querySelector(
+                '[name="down"]').value.toUpperCase()],
+            rightValue = Counts.keyCode[document.querySelector(
+                '[name="right"]').value.toUpperCase()],
+            leftValue = Counts.keyCode[document.querySelector(
+                '[name="left"]').value.toUpperCase()];
+
+        if (upValue && downValue && rightValue && leftValue) {
+            keydownCommands[upValue] = 'moveUp';
+            keydownCommands[rightValue] = 'moveRight';
+            keydownCommands[downValue] = 'moveDown';
+            keydownCommands[leftValue] = 'moveLeft';
+        } else {
+            alert('按键配置有误，请重新配置');
+        }
+    };
 };
 
 module.exports = {
